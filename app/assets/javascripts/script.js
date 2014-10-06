c14540e5597238a0da0ad05e8d1381fe61b52c98
Valentine Toulemonde <valentine.toulemonde@gmail.com>
2014-10-06 13:39:47 -0700

    Use of Google Map API to display place on a map when show a place to create a new visit.

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -2,6 +2,7 @@ function initialize() {
 
   	var place_lat = parseFloat($('#place_lat').val());
   	var place_lng = parseFloat($('#place_lng').val());
+    console.log(place_lat +' '+place_lng)
 
   	// Create the map center on the location or on SF by default
   	if (place_lat && place_lng){
@@ -38,41 +39,43 @@ function initialize() {
       if (recenter){map.setCenter(myLatlng);}
     }
 
-	// Add an event when click on the map to update the address
-	google.maps.event.addListener(map, 'click', function(event) {
-		var click_lat = event.latLng.k;
-		var click_lng = event.latLng.B;
-    console.log('Click '+ click_lat+' '+ click_lng)
-		clearPin();
-		createPin(click_lat, click_lng, false);
-		$('#place_lat').val(click_lat);
-		$('#place_lng').val(click_lng);
-	});
+    if($('#map-canvas').data('map') === 'enable') {
+    	// Add an event when click on the map to update the address
+    	google.maps.event.addListener(map, 'click', function(event) {
+    		var click_lat = event.latLng.k;
+    		var click_lng = event.latLng.B;
+        
+    		clearPin();
+    		createPin(click_lat, click_lng, false);
+    		$('#place_lat').val(click_lat);
+    		$('#place_lng').val(click_lng);
+    	});
 
-	// Update the pin on the map when entering an address
-	$('#pin_map').click(function(e){
-		e.preventDefault();
-	  	var address = $('#place_address').val();
-	  	var geo_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyBMjQW_pHVa_SJleQQX2BC51pJ4UyhVbK0"
-	  	var request = $.ajax({
-	  		url: geo_url,
-	  		type: "GET",
-	  		dataType: "json"
-	  	});
+    	// Update the pin on the map when entering an address
+    	$('#pin_map').click(function(e){
+    		e.preventDefault();
+    	  	var address = $('#place_address').val();
+    	  	var geo_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyBMjQW_pHVa_SJleQQX2BC51pJ4UyhVbK0"
+    	  	var request = $.ajax({
+    	  		url: geo_url,
+    	  		type: "GET",
+    	  		dataType: "json"
+    	  	});
 
-	  	request.done(function(data){
-        console.log('here')
-	  		if(data.results.length == 0){
-	  			alert("INVALID ADDRESS")
-	  		} else {
-	  			var geo_lat = data.results[0].geometry.location.lat;
-	  			var geo_lng = data.results[0].geometry.location.lng;
-	  			clearPin();
-	  			createPin(geo_lat, geo_lng, true);
-          $('#place_lat').val(geo_lat);
-          $('#place_lng').val(geo_lng);
-	  		}
-	  	});
-	});
+    	  	request.done(function(data){
+            console.log('here')
+    	  		if(data.results.length == 0){
+    	  			alert("INVALID ADDRESS")
+    	  		} else {
+    	  			var geo_lat = data.results[0].geometry.location.lat;
+    	  			var geo_lng = data.results[0].geometry.location.lng;
+    	  			clearPin();
+    	  			createPin(geo_lat, geo_lng, true);
+              $('#place_lat').val(geo_lat);
+              $('#place_lng').val(geo_lng);
+    	  		}
+    	  	});
+    	});
+    }
 }
 
